b96dbfe99fd14262a82164ece3905e25238e38d6
rebebba <102855098+rebebba@users.noreply.github.com>
2023-07-31 09:24:32 +0100

    updated fuction descriptions

--- R/data.R
+++ R/data.R
@@ -1,17 +1,17 @@
 #' Star Trek The Original Series - Survival of Starfleet personnel
 #'
-#' A dataset containing the survival of the 430 crew on-board the Enterprise,
-#' including 43 officers (gold uniforms), 151 scientists (blue uniforms),
-#' and 236 members of the operations division (red uniforms)
+#' A dataset containing the survival status (dead or alive) for each of the 430 crew 
+#' members on-board the Enterprise at the end of season 3 and their Starfleet uniform 
+#' color. The crew included 43 officers (gold uniforms), 151 scientists (blue uniforms),
+#' and 236 members of the operations division (red uniforms).
 #'
-#' @format A data frame with 430 rows and 2 variables:
+#' @format A data frame with 430 rows and 2 variables. Each row is a unique crew member.
 #' \describe{
-#'   \item{uniform}{each row is a unique individual and the color of their Starfleet uniform (gold, blue, or red)}
-#'   \item{status}{survival status of each individual at the end of Season 3 (dead or alive)}
+#'   \item{uniform}{the color of the Starfleet uniform (gold, blue, or red) worn by crew member i}
+#'   \item{status}{the survival status at the end of season 3 (dead or alive) of crew member i}
 #' }
 #' @source <https://www.ex-astris-scientia.org/database/redshirt_deaths.htm>
 #' @source <https://memory-gamma.fandom.com/wiki/USS_Enterprise_(NCC-1701)>
 #' 
-#' @examples
-#' data(startrek)
+#' @usage data("startrek")
 "startrek"
